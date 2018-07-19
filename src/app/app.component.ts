adc7746f874612dc36e599c1c1ef2195719a654d
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-19 00:36:39 -0400

    bind data to user model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Vartan', 'v@b.com', 5551231234, '', 'morning', true);
 }
